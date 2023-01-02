7b4f0ee32275e8358943029ebcfeddd0f21338e3
ChingisMut <mchp94@mail.ru>
2023-01-02 16:55:25 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+  let num = Math.abs(parseInt(n));
+  let reverse = num.toString().split('').reverse().join('');
+  console.log(reverse);
+  return Number(reverse);
 }
